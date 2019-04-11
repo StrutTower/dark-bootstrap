80e4bf7dc1c9a37430799697f1d43dee9108974d
Alex Tiller <tilleao@dshs.wa.gov>
2019-04-10 17:11:49 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# Dark Bootstrap Theme
+
+Simple theme that just switches the default Bootstrap 4 theme to a dark theme.
---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+﻿/// <binding ProjectOpened='sass-watch' />
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    cleancss = require('gulp-clean-css'),
+    rename = require('gulp-rename');
+
+var options = {
+    sass: {
+        src: ['src/dark.scss'],
+        files: 'src/**/*.scss',
+        dest: 'dist'
+    }
+};
+
+// Tasks
+gulp.task('sass', function () {
+    return gulp.src(options.sass.src)
+        .pipe(sass({
+            errLogToConsole: true,
+            precision: 10
+        })
+            .on('error', sass.logError))
+        .pipe(gulp.dest(options.sass.dest))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(cleancss({
+            level: { 1: { specialComments: 0 } }
+        }))
+        .pipe(gulp.dest(options.sass.dest));
+});
+
+gulp.task('sass-watch', function () {
+    return gulp.watch(options.sass.files, gulp.series('sass'));
+});
+
+gulp.task('default', gulp.parallel('sass'));
